3cca7d5cb0859beebb79f384195dae0c49a28f79
hsmtkk <hsmtkk@gmail.com>
2023-12-12 14:40:20 +0900

    up

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <p>count {{ counterState.counter }}</p>
+  <button v-on:click="counterState.increment">+</button>
+  <button v-on:click="counterState.decrement">-</button>
+  <button v-on:click="counterState.reset">reset</button>
 </template>
+
+<script setup lang="ts">
+import { useCounterState } from './composables';
+const counterState = useCounterState()
+</script>
--- composables/index.ts
+++ composables/index.ts
@@ -1,3 +1,23 @@
 export const useCounterState = function () {
-    useState("counter", () => 0)
+    const count = useState<number>("count", () => 0)
+    return {
+        counter: readonly(count),
+        increment: increment(count),
+        decrement: decrement(count),
+        reset: reset(count),
+    }
+}
+
+const increment = (count: Ref<number>) => () => {
+    count.value++
+}
+
+const decrement = (count: Ref<number>) => () => {
+    if (count.value > 0) {
+        count.value--
+    }
+}
+
+const reset = (count: Ref<number>) => () => {
+    count.value = 0
 }
